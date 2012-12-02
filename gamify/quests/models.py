4c15b05e5edbf2cfc64fb0a90ada6b701d5d9825
Peter Yao <peteryao916@gmail.com>
2012-12-01 18:23:46 -0800

    Added in new EXP and Models

--- gamify/quests/models.py
+++ gamify/quests/models.py
@@ -17,3 +17,25 @@ class CompleteQuest(models.Model):
     user = models.ForeignKey(User)
     quest = models.ForeignKey(Quest)
     completed = models.DateTimeField(auto_now_add=True)
+
+
+class Interest(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class UserInterests(models.Model):
+    user = models.ForeignKey(User)
+    interest = models.ForeignKey(Interest)
+
+
+class UserAdditions(models.Model):
+    user = models.OneToOneField(User)
+    exp = models.FloatField()
+    occupation = models.CharField(max_length=200)
+    avatar = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+
+
+class Rank(models.Model):
+    name = models.CharField(max_length=200)
+    experience_required = models.FloatField()
