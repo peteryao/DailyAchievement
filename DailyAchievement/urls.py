849120510e71935e9eb933c9a7647bbf3acf09c0
Peter Yao <peteryao916@gmail.com>
2013-03-21 16:29:55 -0700

    Massive update for best practices.

---
+++ DailyAchievement/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import *
+from gamify.models import *
+from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^', include('gamify.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+#To handle static html problems
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
---
+++ gamify/admin.py
@@ -0,0 +1,80 @@
+from gamify.models import Quest, CompleteQuest, Interest, UserInterests, UserAdditions, Rank, Flair, Achievement, Trophy, UserAchievement, UserTrophy, ActiveQuest, Competition, Group, UserGroup
+from django.contrib import admin
+from django.contrib.admin.models import LogEntry
+
+
+class QuestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'point_value', 'image')
+
+
+class CompleteQuestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'quest', 'image', 'completed')
+
+
+class InterestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class UserInterestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'interest')
+
+
+class UserAdditionsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'exp', 'points', 'completed', 'occupation', 'avatar', 'location', 'rank', 'flair')
+
+
+class RankAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'experience_required')
+
+
+class FlairAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'image')
+
+
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'image', 'requirements')
+
+
+class TrophyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'image', 'achievement')
+
+
+class UserAchievementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'achievement', 'date')
+
+
+class UserTrophyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'trophy')
+
+
+class ActiveQuestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'quest', 'user')
+
+
+class CompetitionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_one', 'user_two')
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'avatar')
+
+
+class UserGroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'group', 'user')
+
+admin.site.register(LogEntry)
+admin.site.register(Quest, QuestAdmin)
+admin.site.register(CompleteQuest, CompleteQuestAdmin)
+admin.site.register(Interest, InterestAdmin)
+admin.site.register(UserInterests, UserInterestAdmin)
+admin.site.register(UserAdditions, UserAdditionsAdmin)
+admin.site.register(Rank, RankAdmin)
+admin.site.register(Flair, FlairAdmin)
+admin.site.register(Achievement, AchievementAdmin)
+admin.site.register(Trophy, TrophyAdmin)
+admin.site.register(UserAchievement, UserAchievementAdmin)
+admin.site.register(UserTrophy, UserTrophyAdmin)
+admin.site.register(ActiveQuest, ActiveQuestAdmin)
+admin.site.register(Competition, CompetitionAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(UserGroup, UserGroupAdmin)
